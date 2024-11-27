a1bcbbae4c6132b45d8f7b215043b07381c4e721
devjiwonchoi <devjiwonchoi@gmail.com>
2024-11-28 00:45:10 +0900

    use persistence cache

--- apps/one/next.config.ts
+++ apps/one/next.config.ts
@@ -2,8 +2,12 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   experimental: {
+    dynamicIO: true,
     ppr: true,
     typedEnv: true,
+    turbo: {
+      unstablePersistentCaching: true,
+    },
   },
 }
 
